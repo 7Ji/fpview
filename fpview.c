8e8dfdc6f79285fa9ce2af2092cb5d5ef923efeb
Guoxin Pu <pugokushin@gmail.com>
2025-06-26 17:24:43 +0800

    also print in-part bit offset

--- fpview.c
+++ fpview.c
@@ -31,10 +31,16 @@
 #define COLOR_FRACTION      COLOR_RED
 
 #define TEXT_SEPERATOR "================================================================================================================================================================================================"
-#define TEXT_31_TO_0   "31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0"
-#define TEXT_63_TO_32  "63 62 61 60 59 58 57 56 55 54 53 52 51 50 49 48 47 46 45 44 43 42 41 40 39 38 37 36 35 34 33 32"
+#define TEXT_7_TO_3    " 7  6  5  4  3"
+#define TEXT_10_TO_3   "10  9  8 " TEXT_7_TO_3
+#define TEXT_22_TO_3   "22 21 20 19 18 17 16 15 14 13 12 11 " TEXT_10_TO_3
+#define TEXT_31_TO_3   "31 30 29 28 27 26 25 24 23 " TEXT_22_TO_3
+#define TEXT_2_TO_0    " 2  1  0"
+#define TEXT_31_TO_0   TEXT_31_TO_3 TEXT_2_TO_0
+#define TEXT_51_TO_32  "51 50 49 48 47 46 45 44 43 42 41 40 39 38 37 36 35 34 33 32"
+#define TEXT_51_TO_3   TEXT_51_TO_32 " 31 30 29 28 27 26 25 24 23 " TEXT_22_TO_3
+#define TEXT_63_TO_32  "63 62 61 60 59 58 57 56 55 54 53 52 " TEXT_51_TO_32
 #define TEXT_63_TO_0    TEXT_63_TO_32 " " TEXT_31_TO_0
-
 
 int main(int argc, char *argv[]) {
     struct option const long_options[] = {
@@ -138,7 +144,7 @@ int main(int argc, char *argv[]) {
             for (short i = SINGLE_OFF_FRACTION_HIGH; i >= SINGLE_OFF_FRACTION_LOW; --i) {
                 printf("  %hhu", digits[i]);
             }
-            printf(COLOR_NONE"\n"COLOR_SIGN"sign"COLOR_NONE"       "COLOR_EXPONENT"exponent"COLOR_NONE"                                      "COLOR_FRACTION"fraction"COLOR_NONE"\n");
+            printf(COLOR_NONE"\n"COLOR_SIGN"sign"COLOR_EXPONENT TEXT_7_TO_3 " exponent "COLOR_FRACTION TEXT_22_TO_3" fraction"COLOR_NONE"\n");
         }
         if (format & FloatingFormatDouble) {
             printf(TEXT_SEPERATOR"\n%s as double-precision: ", argv[i]);
@@ -182,7 +188,7 @@ int main(int argc, char *argv[]) {
             for (short i = DOUBLE_OFF_FRACTION_HIGH; i >= DOUBLE_OFF_FRACTION_LOW; --i) {
                 printf("  %hhu", digits[i]);
             }
-            printf(COLOR_NONE"\n"COLOR_SIGN"sign"COLOR_NONE"            "COLOR_EXPONENT"exponent"COLOR_NONE"                                                                                      "COLOR_FRACTION"fraction"COLOR_NONE"\n");
+            printf(COLOR_NONE"\n"COLOR_SIGN"sign"COLOR_EXPONENT TEXT_10_TO_3" exponent "COLOR_FRACTION TEXT_51_TO_3" fraction"COLOR_NONE"\n");
         }
     }
     puts(TEXT_SEPERATOR);
